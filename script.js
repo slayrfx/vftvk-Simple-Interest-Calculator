f9d852da1bae85beb5296ccede219a2cc55149c5
slayrfx <64538601+slayrfx@users.noreply.github.com>
2021-06-22 21:36:27 +0530

    Update script.js
    Highlighted the values using <mark> tag.

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     var interest = Number((principal*years*rate)/100);
     var amount = Number(principal+interest);
-    document.getElementById("result").innerHTML="<br><br>If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+".\<br\>"
+    document.getElementById("result").innerHTML="<br><br>If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>.\<br\>"
     
         
 }
